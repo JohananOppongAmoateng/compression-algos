2627b1303309abe359a9244686151227f2d1fc21
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2026-07-19 14:08:48 +0000

    Implement Run-Length Encoding (RLE) and Decoding functions in main.c

--- main.c
+++ main.c
@@ -1,14 +1,69 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* TODO (frequency): implement per the lesson description. */
+static void strip_newline(char *s) {
+    size_t n = strlen(s);
+    if (n > 0 && s[n - 1] == '\n')
+        s[n - 1] = '\0';
+}
+
+static void rle_encode(const char *s) {
+    if (*s == '\0') {
+        printf("\n");
+        return;
+    }
+
+    int first = 1;
+    while (*s) {
+        char c = *s;
+        int count = 0;
+        while (*s == c) {
+            count++;
+            s++;
+        }
+        if (!first)
+            putchar(' ');
+        first = 0;
+        printf("%d%c", count, c);
+    }
+    putchar('\n');
+}
+
+static void rle_decode(const char *s) {
+    while (*s) {
+        while (*s == ' ')
+            s++;
+        if (*s == '\0')
+            break;
+
+        int count = 0;
+        while (isdigit((unsigned char)*s)) {
+            count = count * 10 + (*s - '0');
+            s++;
+        }
+        if (*s == '\0')
+            break;
+
+        char c = *s++;
+        for (int i = 0; i < count; i++)
+            putchar(c);
+    }
+    putchar('\n');
+}
 
 int main(void) {
     char line[1024];
     while (fgets(line, sizeof line, stdin)) {
-        if (line[0] == '\n' || line[0] == 0) continue;
-        printf("TODO\n");
+        strip_newline(line);
+        if (line[0] == '\0')
+            continue;
+
+        if (strncmp(line, "ENCODE ", 7) == 0)
+            rle_encode(line + 7);
+        else if (strncmp(line, "DECODE ", 7) == 0)
+            rle_decode(line + 7);
     }
     return 0;
 }
